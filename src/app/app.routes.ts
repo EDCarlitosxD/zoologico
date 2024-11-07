c4bb9bc99ccd52a31c9dd6cd3f1b832d76fa29b4
AdriHB-02 <adrian.hdz.bdz@gmail.com>
2024-11-07 13:47:01 -0500

    Feat(Animales): Se agrego la pagina animales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { DonacionesComponent } from './pages/donaciones/donaciones.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AnimalesComponent } from './pages/animales/animales.component';
 
 
 export const routes: Routes = [
-    {path: 'donaciones', component: DonacionesComponent}  
+    {path: 'donaciones', component: DonacionesComponent}, 
+    {path: '', component: HomeComponent},
+    {path: 'animales', component:AnimalesComponent}
 ];
 
