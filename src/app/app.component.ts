be532ffa563760b2f63d1b81ccc847e22b32bc6c
EDCarlitosxD <juanuchdzib@gmail.com>
2024-11-12 07:10:34 -0500

    Fix(Footer) Footer complete, responsive mobile and desktop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,35 +4,22 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 //COMPONENTES
 import { AnimalCardComponent } from './Componentes/animal-card/animal-card.component';
 import { ExperienciasCardComponent } from './Componentes/experiencias-card/experiencias-card.component';
-import { TextGrid4Component } from './Componentes/text-component/text-grid4/text-grid4.component'; 
+import { TextGrid4Component } from './Componentes/text-component/text-grid4/text-grid4.component';
 import { TextGrid5Component } from './Componentes/text-component/text-grid5/text-grid5.component';
 import { BoletosComponent } from './Componentes/boletos/boletos.component';
 import { FooterComponent } from './Componentes/footer/footer.component';
 import { NavBarComponent } from "./Componentes/nav-bar/nav-bar.component";
-import { SliderComponent } from "./Componentes/slider/slider.component";
 
 //PAGES
-import { DonacionesComponent } from "./pages/donaciones/donaciones.component";
-import { AnimalesComponent } from './pages/animales/animales.component';
-import { HomeComponent } from './pages/home/home.component';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet,
-    RouterLink,
-
-    AnimalCardComponent,
-    ExperienciasCardComponent,
-    TextGrid4Component,
-    TextGrid5Component,
-    BoletosComponent,
-    FooterComponent,
     NavBarComponent,
-    SliderComponent,
-
-    HomeComponent,
-    AnimalesComponent,
-    DonacionesComponent],
+    FooterComponent,
+    ],
 
 
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,12 @@
 import { Routes } from '@angular/router';
 import { DonacionesComponent } from './pages/donaciones/donaciones.component';
-import { HomeComponent } from './pages/home/home.component';
 import { AnimalesComponent } from './pages/animales/animales.component';
+import { HomeComponent } from './pages/home/home.component';
 
 
 export const routes: Routes = [
-    {path: 'donaciones', component: DonacionesComponent}, 
-    {path: '', component: HomeComponent},
-    {path: 'animales', component:AnimalesComponent}
+    {path: 'donaciones', component: DonacionesComponent},
+    {path: 'animales', component:AnimalesComponent},
+    {path: '', component:HomeComponent}
 ];
 
