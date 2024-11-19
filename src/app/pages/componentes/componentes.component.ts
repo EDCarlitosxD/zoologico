524492cd8e030a51800f280909c955674b2fa18c
EDCarlitosxD <juanuchdzib@gmail.com>
2024-11-18 23:38:50 -0500

    Feat(Home) Home Page Mobile. Note: Scroll not working 100%, Slider and Experiencias section not exist

--- src/app/pages/componentes/componentes.component.ts
+++ src/app/pages/componentes/componentes.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { BoletoComponent } from "../../Componentes/boleto/boleto.component";
+import { RecorridoHomeComponent } from "../../Componentes/recorrido-home/recorrido-home.component";
+
+
 
 @Component({
   selector: 'app-componentes',
   standalone: true,
-  imports: [BoletoComponent],
+  imports: [BoletoComponent, RecorridoHomeComponent],
   templateUrl: './componentes.component.html',
   styleUrl: './componentes.component.scss'
 })
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { AnimalCardComponent } from "../../Componentes/animal-card/animal-card.component";
+import { BoletoComponent } from "../../Componentes/boleto/boleto.component";
+import { RecorridoHomeComponent } from "../../Componentes/recorrido-home/recorrido-home.component";
+import { FooterComponent } from "../../Componentes/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [AnimalCardComponent, BoletoComponent, RecorridoHomeComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
