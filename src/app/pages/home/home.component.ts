94ff3b924366003313d467f9aeea5a7d9ba69a54
EDCarlitosxD <juanuchdzib@gmail.com>
2024-11-21 07:47:41 -0500

    Fix(Home) Hero fixend overoposition

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { BoletoComponent } from "../../Componentes/boleto/boleto.component";
 import { RecorridoHomeComponent } from "../../Componentes/recorrido-home/recorrido-home.component";
 import { FooterComponent } from "../../Componentes/footer/footer.component";
 import { NavBarComponent } from "../../Componentes/nav-bar/nav-bar.component";
+import { SliderComponent } from "../../Componentes/slider/slider.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AnimalCardComponent, BoletoComponent, RecorridoHomeComponent, FooterComponent, NavBarComponent],
+  imports: [AnimalCardComponent, BoletoComponent, RecorridoHomeComponent, FooterComponent, NavBarComponent, SliderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
