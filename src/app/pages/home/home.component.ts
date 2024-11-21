026410d347f436d05bfcad838f27488d6c4f787a
EDCarlitosxD <juanuchdzib@gmail.com>
2024-11-20 22:23:02 -0500

    Fear(Home) Pagina Home terminada, pero con no esta a pixel en las ultimas secciones y los sliders no funcionan

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,12 @@ import { AnimalCardComponent } from "../../Componentes/animal-card/animal-card.c
 import { BoletoComponent } from "../../Componentes/boleto/boleto.component";
 import { RecorridoHomeComponent } from "../../Componentes/recorrido-home/recorrido-home.component";
 import { FooterComponent } from "../../Componentes/footer/footer.component";
+import { NavBarComponent } from "../../Componentes/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [AnimalCardComponent, BoletoComponent, RecorridoHomeComponent, FooterComponent],
+  imports: [AnimalCardComponent, BoletoComponent, RecorridoHomeComponent, FooterComponent, NavBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
