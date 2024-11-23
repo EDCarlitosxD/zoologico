2d593445bd617eb28997282f75a3150e2c3d4db2
EDCarlitosxD <juanuchdzib@gmail.com>
2024-11-22 20:29:46 -0500

    Fixeds home

--- src/app/pages/animales/animales.component.ts
+++ src/app/pages/animales/animales.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AnimalCardComponent } from "../../Componentes/animal-card/animal-card.component";
+import { NavBarComponent } from "../../Componentes/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../Componentes/footer/footer.component";
 
 @Component({
   selector: 'app-animales',
   standalone: true,
-  imports: [AnimalCardComponent],
+  imports: [AnimalCardComponent, NavBarComponent, FooterComponent],
   templateUrl: './animales.component.html',
   styleUrl: './animales.component.scss'
 })
--- src/app/pages/boletos/boletos.component.ts
+++ src/app/pages/boletos/boletos.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AddBoletosComponent } from "../../Componentes/add-boletos/add-boletos.component";
+import { NavBarComponent } from "../../Componentes/nav-bar/nav-bar.component";
+import { FooterComponent } from "../../Componentes/footer/footer.component";
 
 @Component({
   selector: 'app-boletos',
   standalone: true,
-  imports: [AddBoletosComponent],
+  imports: [AddBoletosComponent, NavBarComponent, FooterComponent],
   templateUrl: './boletos.component.html',
   styleUrls: ['./boletos.component.scss']
 })
