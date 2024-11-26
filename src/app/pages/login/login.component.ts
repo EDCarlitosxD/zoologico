d0904143a7825098ec19137ca2fa85ea4d6a3abf
AdriHB-02 <adrian.hdz.bdz@gmail.com>
2024-11-26 16:25:19 -0500

    Pagina boletos añadida

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { LoginFormComponent } from "../../Componentes/login-form/login-form.component";
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [LoginFormComponent],
+  imports: [LoginFormComponent,],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  constructor(private location: Location) {}
 
+  goBack(): void {
+    this.location.back(); // Navega a la página anterior en el historial
+  }
 }
