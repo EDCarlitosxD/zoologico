a40f38b7f30d3ce2100a5636d1281d99124c0f0f
AdriHB-02 <adrian.hdz.bdz@gmail.com>
2024-11-28 08:00:09 -0500

    feat cierre

---
+++ src/app/pages/cierre/cierre.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cierre',
+  standalone: true,
+  imports: [],
+  templateUrl: './cierre.component.html',
+  styleUrl: './cierre.component.scss'
+})
+export class CierreComponent {
+
+}
