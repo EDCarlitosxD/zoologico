ddb14490fe31248058c82fc16b534b921f490894
AdriHB-02 <adrian.hdz.bdz@gmail.com>
2024-11-27 21:26:52 -0500

    fix home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,11 +6,19 @@ import { FooterComponent } from "../../Componentes/footer/footer.component";
 import { NavBarComponent } from "../../Componentes/nav-bar/nav-bar.component";
 import { SliderComponent } from "../../Componentes/slider/slider.component";
 import { RouterLink } from '@angular/router';
+import { ExperienciasComponent } from "../../Componentes/modals/experiencias/experiencias.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, AnimalCardComponent, BoletoComponent, RecorridoHomeComponent, FooterComponent, NavBarComponent, SliderComponent],
+  imports: [RouterLink, 
+    AnimalCardComponent, 
+    BoletoComponent, 
+    RecorridoHomeComponent, 
+    FooterComponent, 
+    NavBarComponent, 
+    SliderComponent, 
+    ExperienciasComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
