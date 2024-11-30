822ce781df0c5b797f5c3e2ad0abea40fbd9dfc8
AdriHB-02 <adrian.hdz.bdz@gmail.com>
2024-11-30 01:00:35 -0500

    fix pagar y cierre: se agrego el indicador

--- src/app/pages/cierre/cierre.component.ts
+++ src/app/pages/cierre/cierre.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { IndicadorComponent } from "../../Componentes/indicador/indicador.component";
 
 @Component({
   selector: 'app-cierre',
   standalone: true,
-  imports: [],
+  imports: [IndicadorComponent],
   templateUrl: './cierre.component.html',
   styleUrl: './cierre.component.scss'
 })
--- src/app/pages/pagar/pagar.component.ts
+++ src/app/pages/pagar/pagar.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { AtrasBtnComponent } from "../../Componentes/atras-btn/atras-btn.component";
+import { ContadorResumenPedidoComponent } from "../../Componentes/contador-resumen-pedido/contador-resumen-pedido.component";
+import { IndicadorComponent } from "../../Componentes/indicador/indicador.component";
 
 @Component({
   selector: 'app-pagar',
   standalone: true,
-  imports: [AtrasBtnComponent],
+  imports: [AtrasBtnComponent, ContadorResumenPedidoComponent, IndicadorComponent],
   templateUrl: './pagar.component.html',
   styleUrl: './pagar.component.scss'
 })
