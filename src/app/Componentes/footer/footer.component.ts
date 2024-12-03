d96669f9953f31d39a2d8f9382ff18ff43149a29
AdriHB-02 <adrian.hdz.bdz@gmail.com>
2024-12-03 17:41:55 -0500

    fix footer: se agregaron links yy footer a pixel

--- src/app/Componentes/footer/footer.component.ts
+++ src/app/Componentes/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 interface Footer{
 
 }
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
