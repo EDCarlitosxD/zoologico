2edcbeb120d51f32d5ebcb4aedebaf432b51d972
AdriHB-02 <adrian.hdz.bdz@gmail.com>
2024-12-05 20:22:57 -0500

    fix recorridos: Se arreglo el flotante

--- src/app/pages/recorridos/recorridos.component.ts
+++ src/app/pages/recorridos/recorridos.component.ts
@@ -8,14 +8,23 @@ import { TourComponentComponent } from "../../Componentes/tour-component/tour-co
 import { RouterLink } from '@angular/router';
 import { BolDisponiblesComponent } from "../../Componentes/bol-disponibles/bol-disponibles.component";
 import { WarningComponent } from "../../Componentes/warning/warning.component";
-
+import { NgFor } from '@angular/common';
+interface Boletos {
+  type: string;
+  price: number;
+  cantidad: number;
+}
 @Component({
   selector: 'app-recorridos',
   standalone: true,
-  imports: [RouterLink, NavBarComponent, FooterComponent, SelectorRecorridoComponent, IndicadorComponent, AddBoletosComponent, TourComponentComponent, BolDisponiblesComponent, WarningComponent],
+  imports: [NgFor, RouterLink, NavBarComponent, FooterComponent, SelectorRecorridoComponent, IndicadorComponent, AddBoletosComponent, TourComponentComponent, BolDisponiblesComponent, WarningComponent],
   templateUrl: './recorridos.component.html',
   styleUrl: './recorridos.component.scss'
 })
 export class RecorridosComponent {
-
+  bols: Boletos[] = [
+    { type: 'Infantil', price: 70, cantidad: 0 },
+    { type: 'Adultos', price: 120, cantidad: 1 },
+    { type: 'Tercera Edad', price: 70, cantidad: 0 },
+  ]
 }
