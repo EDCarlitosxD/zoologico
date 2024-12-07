f1802d425af4ec3c2c391928d9650b9266c32eea
EDCarlitosxD <juanuchdzib@gmail.com>
2024-12-07 18:48:01 -0500

    Fix Fix post merge and create ckae chart component (empty)

---
+++ src/app/Componentes/chart-cake/chart-cake.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-cake',
+  standalone: true,
+  imports: [],
+  templateUrl: './chart-cake.component.html',
+  styleUrl: './chart-cake.component.scss'
+})
+export class ChartCakeComponent {
+
+}
