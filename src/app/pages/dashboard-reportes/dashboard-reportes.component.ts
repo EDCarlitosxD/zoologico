b7e101c7585ed469309f4fea3344e42a8198fd30
AdriHB-02 <adrian.hdz.bdz@gmail.com>
2024-12-06 23:50:13 -0500

    feat reportes

--- src/app/pages/dashboard-reportes/dashboard-reportes.component.ts
+++ src/app/pages/dashboard-reportes/dashboard-reportes.component.ts
@@ -1,14 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DashboardComponent } from "../Admin/dashboard/dashboard.component";
 import { DashboardContentComponent } from "../../Componentes/Admin/dashboard-content/dashboard-content.component";
+import { NgClass, NgFor } from '@angular/common';
+import { DashboardVentasComponent } from '../dashboard-ventas/dashboard-ventas.component';
+
+interface Boletos{
+  tipo: string;
+  ventas: number;
+  precio: number;
+}
+
+interface Tours{
+  tour: string;
+  ventas: number;
+  precio: number;
+}
 
 @Component({
   selector: 'app-dashboard-reportes',
   standalone: true,
-  imports: [DashboardComponent, DashboardContentComponent],
+  imports: [DashboardContentComponent, NgFor, NgClass],
   templateUrl: './dashboard-reportes.component.html',
   styleUrl: './dashboard-reportes.component.scss'
 })
 export class DashboardReportesComponent {
+  @Input() boletos: Boletos[] = [
+    { tipo: 'Infantil', ventas: 50, precio: 70 },
+    { tipo: 'Adultos', ventas: 120, precio: 120 },
+    { tipo: 'Tercera Edad', ventas: 80, precio: 70 },
+  ];
 
+  @Input () tours: Tours[] = [
+    { tour: 'Safari Fotográfico', ventas: 120, precio: 500 },
+    { tour: 'Espectáculo de aves', ventas: 80, precio: 300 },
+    { tour: 'Alimentación de animales', ventas: 150, precio: 200 },
+    { tour: 'Recorrido nocturno', ventas: 50, precio: 600 },
+    { tour: 'Exploración de selva', ventas: 70, precio: 400 },
+    { tour: 'Visita guiada', ventas: 200, precio: 250 }
+  ];
+
+  getTotalBoletos(): number {
+    return this.boletos.reduce((total, boleto) => total + boleto.ventas, 0);
+  }
+
+  getTotalTours(): number {
+    return this.tours.reduce((total, tour) => total + tour.ventas, 0);
+  }
 }
